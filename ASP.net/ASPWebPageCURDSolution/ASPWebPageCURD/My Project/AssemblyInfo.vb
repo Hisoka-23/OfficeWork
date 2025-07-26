c4ca11d40e4e1ec00f1321d200eddec500a5e898
prakash <prakash.chaurasiya214522@gmail.com>
2025-07-26 18:43:39 +0530

    somethings is added

---
+++ ASP.net/ASPWebPageCURDSolution/ASPWebPageCURD/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("ASPWebPageCURD")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ASPWebPageCURD")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1ff19343-4027-4424-92e6-b2b956af6c03")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ ASP.net/ASPWebPageCURDSolution/ASPWebPageCURD/WebPage/HTML/Main.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/WebPage/HTML/Base.Master" CodeBehind="Main.aspx.vb" Inherits="ASPWebPageCURD.Main" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div>
+        <h1 class="hello">
+            Hello world
+        </h1>
+    </div>
+
+</asp:Content>

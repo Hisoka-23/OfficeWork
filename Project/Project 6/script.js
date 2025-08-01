0f8da53cd2e459e740c121f8bbea946533b64bf6
prakash <prakash.chaurasiya214522@gmail.com>
2025-08-01 10:52:40 +0530

    project 6 is complete

--- Project/Project 6/script.js
+++ Project/Project 6/script.js
@@ -9,18 +9,28 @@ gsap.from("#img2",{
     delay:0.4,
     opacity:0,
     duration:1,
-    x:60
-})
+    x:100
+}, "-=0.8")
 
 gsap.from("#img3",{
     delay:0.4,
     opacity:0,
     duration:1,
-    x:-60
-})
+    y:-60
+}, "-=0.8")
 
 gsap.from("#main h1",{
     delay:0.4,
     opacity:0,
     duration:1
+}, "-=0.8")
+
+gsap.from("#page2 h5, #page2 h1, #page2 #about-us", {
+    opacity:0,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#page2 h5",
+        scroller:"#body",
+        start:"top 60%",
+    }
 })
